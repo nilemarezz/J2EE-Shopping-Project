5fcbb2b98723eb11c793b2335112d065db51a96f
NilE <nilenon@gmail.com>
2018-11-29 19:49:29 +0700

    [Add]Filter

---
+++ MyProjectWebApp/src/java/Logger/LoginFilter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logger;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Nile
+ */
+public class LoginFilter implements Filter {
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest)request).getSession(false);
+        if(session.getAttribute("username") != null){
+            chain.doFilter(request, response);  
+            return;
+        }else{
+            filterConfig.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+        }    
+    }
+
+    @Override
+    public void destroy() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
